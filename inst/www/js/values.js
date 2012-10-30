e06c2819444224dd10ee3946efed387b8f9a827d
Richard D. Morey <richarddmorey@gmail.com>
2012-10-30 00:00:00 +0000

    version 0.8.5

---
+++ inst/www/js/values.js
@@ -0,0 +1,16 @@
+var progressPollTime = 1000; // time in ms
+
+var roundTo;
+
+var advCols = [ 0, 6, 7, 8, 9, 10, 11 ];
+var intervalRefs;
+
+var base;
+var textBase;
+var bayesFactors = [ ];
+var nFac;
+
+var progressWidth = 200;
+var progressHeight = 50;
+
+
